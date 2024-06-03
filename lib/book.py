715e5061c084139bfaa8df86d45a227ad0353e8c
naliali <nalialiangela365@gmail.com>
2024-06-03 12:58:17 +0300

    initial comit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,34 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None
+        self.page_count = page_count  
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 100:
+            self._title = value
+        else:
+            raise ValueError("Title must be a string between 1 and 100 characters.")
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int) and value > 0:
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None
+        self.size = size  
+        self.condition = "Used"  
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 50:
+            self._brand = value
+        else:
+            raise ValueError("Brand must be a string between 1 and 50 characters.")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
